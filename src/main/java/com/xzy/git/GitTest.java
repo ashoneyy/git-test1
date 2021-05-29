0817af9fd9951ecb1003b05d8cc7046377c71315
xzy <n6450451@163.com>
2021-05-29 16:38:48 +0800

    hot-fix commit1

--- src/main/java/com/xzy/git/GitTest.java
+++ src/main/java/com/xzy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!!");
         System.out.println("hello git22!!");
         System.out.println("hello git33!!");
+        System.out.println("hello git44!!");
     }
 }
