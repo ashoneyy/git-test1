29a2d29f2d049bb3dbd23be268bbd3fa0768d43d
xzy <n6450451@163.com>
2021-05-29 14:26:37 +0800

    first commit

---
+++ src/main/java/com/xzy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xzy.git;
+
+/**
+ * @author xzy
+ * @create 2021 14:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+}
